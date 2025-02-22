a1fb4afe37b7cf90ff3c9df4196e7dbec45868d5
Abe Clements <abram.j.clements@gmail.com>
2025-02-22 13:08:31 -0700

    init sensor example

---
+++ sensor/src/main.rs
@@ -0,0 +1,28 @@
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+use embedded_hal::{delay::DelayNs, digital::OutputPin};
+use microbit::{hal::Timer, Board};
+use panic_halt as _;
+use rtt_target::{rprintln, rtt_init_print};
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    let mut board = Board::take().unwrap();
+
+    let mut timer = Timer::new(board.TIMER0);
+
+    board.display_pins.col1.set_low().unwrap();
+    board.display_pins.col5.set_low().unwrap();
+    let mut row1 = board.display_pins.row1;
+    loop {
+        row1.set_low().unwrap();
+        rprintln!("Dark!");
+        timer.delay_ms(1_000);
+        row1.set_high().unwrap();
+        rprintln!("Light!");
+        timer.delay_ms(1_000);
+    }
+}
