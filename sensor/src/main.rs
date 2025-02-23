394ac51339e65767b7e8f926f77ec9aa48f03d04
Abe Clements <abram.j.clements@gmail.com>
2025-02-22 22:23:15 -0700

    send accelerometer data over rtt

--- sensor/src/main.rs
+++ sensor/src/main.rs
@@ -1,10 +1,13 @@
 #![no_main]
 #![no_std]
 
+use core::fmt::{self, Write};
+
 use cortex_m_rt::entry;
-use embedded_hal::{delay::DelayNs, digital::OutputPin};
+use heapless::{String, Vec};
+use lsm303agr::{AccelOutputDataRate, Lsm303agr};
 use microbit::{
-    hal::{Timer, Uarte},
+    hal::{twim, uarte, Timer, Uarte},
     Board,
 };
 use panic_halt as _;
@@ -13,26 +16,69 @@ use rtt_target::{rprintln, rtt_init_print};
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    let mut board = Board::take().unwrap();
+    let board = Board::take().unwrap();
 
     let mut timer = Timer::new(board.TIMER0);
+    let mut accel_delay_timer = Timer::new(board.TIMER2);
 
-    let serial = Uarte::new(
+    let mut serial = Uarte::new(
         board.UARTE0,
         board.uart.into(),
-        microbit::hal::uarte::Parity::INCLUDED,
+        microbit::hal::uarte::Parity::EXCLUDED,
         microbit::hal::uarte::Baudrate::BAUD115200,
     );
 
-    board.display_pins.col1.set_low().unwrap();
-    board.display_pins.col5.set_low().unwrap();
-    let mut row1 = board.display_pins.row1;
+    let i2c = twim::Twim::new(
+        board.TWIM0,
+        board.i2c_internal.into(),
+        twim::Frequency::K100,
+    );
+    let mut accel_sensor = Lsm303agr::new_with_i2c(i2c);
+    accel_sensor.init().unwrap();
+    accel_sensor
+        .set_accel_mode_and_odr(
+            &mut accel_delay_timer,
+            lsm303agr::AccelMode::LowPower,
+            AccelOutputDataRate::Hz1,
+        )
+        .unwrap();
+
+    let mut read_buf = [0u8; 128];
+
     loop {
-        row1.set_low().unwrap();
-        rprintln!("Dark!");
-        timer.delay_ms(1_000);
-        row1.set_high().unwrap();
-        rprintln!("Light!");
-        timer.delay_ms(1_000);
+        if let Ok(status) = accel_sensor.accel_status() {
+            if status.xyz_new_data() {
+                let data = accel_sensor.acceleration().unwrap();
+                let (x, y, z) = data.xyz_mg();
+                let msg = build_msg(x, y, z);
+                rprintln!("message: {}", msg);
+            }
+        } else {
+            rprintln!("couldn't check accelerometer status");
+        }
+
+        match serial.read_timeout(&mut read_buf, &mut timer, 1000) {
+            Ok(_) => rprintln!("recieved: {:?}", &read_buf),
+            Err(uarte::Error::Timeout(n)) => {
+                if n > 0 {
+                    rprintln!("recieved bytes: {:?}", &read_buf[..n]);
+                }
+            }
+            Err(e) => {
+                rprintln!("recieved_error: {:?}", e)
+            }
+        }
     }
+}
+
+fn build_msg(x: i32, y: i32, z: i32) -> String<251> {
+    let mut data: String<251> = String::new();
+    write!(
+        &mut data,
+        "{{\"name\": \"example_sensor\", \"accel_x\": {}, \"accel_y\": {}, \"accel_z\": {}}}",
+        x, y, z
+    )
+    .unwrap();
+
+    data
 }
